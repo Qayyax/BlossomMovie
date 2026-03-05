f93eee937158c9136af0056399c169a9e205b4ae
Qayyax <qayyax@gmail.com>
2026-03-05 16:42:05 -0500

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-03-05.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
