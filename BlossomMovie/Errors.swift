9bee1e22b6286d8e4de5b55bfd2f88089cca849b
Qayyax <qayyax@gmail.com>
2026-03-07 14:52:39 -0500

    preparing to make fetch request

---
+++ BlossomMovie/Errors.swift
@@ -0,0 +1,39 @@
+//
+//  Errors.swift
+//  BlossomMovie
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-03-07.
+//
+
+import Foundation
+
+enum APIConfigError: Error, LocalizedError {
+    case fileNotFound
+    case dataLoadingFailed(underlyingError: Error)
+    case decodingFailed(underlyingError: Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .fileNotFound:
+            return "The file was not found."
+        case .dataLoadingFailed(underlyingError: let error):
+            return "Failed to load data with error: \(error.localizedDescription)"
+        case .decodingFailed(underlyingError: let error):
+            return "Failed to decode data with error: \(error.localizedDescription)"
+        }
+    }
+}
+
+enum NetworkError: Error, LocalizedError {
+    case badURLResponse(underlyingError: Error)
+    case missingConfig
+    
+    var errorDescription: String? {
+        switch self {
+        case .badURLResponse(underlyingError: let error):
+            return "The URL session returned an invalid response with error: \(error.localizedDescription)"
+        case .missingConfig:
+            return "The API configuration is missing."
+        }
+    }
+}
